c5c8e77bc00d26fabd7b542c23b249cee61737bc
Angel Ponce <acdpf000@gmail.com>
2023-06-29 15:24:36 -0600

    fix mouse focus component

--- src/components/atoms/MouseFocus.svelte
+++ src/components/atoms/MouseFocus.svelte
@@ -2,17 +2,25 @@
 	import { spring } from 'svelte/motion';
 
 	const coords = spring({ x: 100, y: 100 });
-	const ratio = spring(15);
+	const ratio = spring(30);
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<svg
-	class="fixed w-full h-screen opacity-50 bg-transparent"
+<svelte:window
 	on:mousemove={(e) => {
-		coords.set({ x: e.clientX, y: e.clientY });
+		coords.set({
+			x: e.clientX - 6,
+			y: e.clientY - 6
+		});
 	}}
-	on:mousedown={() => ratio.set(30)}
-	on:mouseup={() => ratio.set(15)}
->
-	<circle class="fill-[#0C4160]" cx={$coords.x} cy={$coords.y} r={$ratio} />
-</svg>
+	on:mousedown={() => ratio.set(60)}
+	on:mouseup={() => ratio.set(30)}
+/>
+
+<div
+	class="rounded-full bg-dark-base opacity-50 fixed"
+	style:top={`${$coords.y}px`}
+	style:left={`${$coords.x}px`}
+	style:width={`${$ratio}px`}
+	style:height={`${$ratio}px`}
+/>
