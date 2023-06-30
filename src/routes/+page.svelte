17eace2f6d38153d78f878093747cb341ee04297
Angel Ponce <acdpf000@gmail.com>
2023-06-30 01:01:28 -0600

    edit page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,7 @@
 	import { Button } from '$atoms';
 </script>
 
-<p class="font-lilita text-8xl mt-10">Hola mundo</p>
+<p class="font-lilita text-8xl my-10">Angel Ponce</p>
 <p class="text-2xl text-justify">
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iusto deleniti magni laboriosam,
-	cumque aspernatur tempore voluptate quisquam impedit, reprehenderit officia commodi quaerat fuga
-	quis rerum, error ratione quod minima. Aspernatur et repellat, animi voluptates accusantium
-	impedit odio amet incidunt repellendus minima laboriosam dolores nulla esse, voluptatem distinctio
-	harum quam, debitis ea ullam? Libero fugit aliquid, optio quo dolorem nemo? Earum magnam
-	distinctio explicabo officiis ratione odit blanditiis impedit labore, hic aliquid quasi ab
-	pariatur qui. Velit rem sit autem unde? Laudantium modi placeat nihil optio alias quo assumenda
-	odio? Ad id maxime aperiam blanditiis error, incidunt deserunt distinctio laborum quia, aliquam
-	voluptatibus tempora pariatur cumque enim harum, repudiandae eligendi nisi consequatur adipisci
-	nesciunt impedit dolorem deleniti consequuntur unde! Blanditiis! Quis voluptates expedita repellat
-	architecto accusantium quos ut.
+	FullStack developer, enfocado en desarrollar aplicaciones de alta calidad y prestigio.
 </p>
-
-<Button className="mt-4" color="primary">botón 1</Button>
-<Button className="mt-4" color="secondary">botón 2</Button>
-<Button className="mt-4" color="ghost">botón 3</Button>
