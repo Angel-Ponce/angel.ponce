0086848e7f446855a1b7aebeea73d56e41d89661
Angel Ponce <acdpf000@gmail.com>
2023-07-04 10:06:07 -0600

    angel avatar dark mode

--- src/components/organisms/AboutMe.svelte
+++ src/components/organisms/AboutMe.svelte
@@ -3,7 +3,7 @@
 	import t from '$i18n/t';
 </script>
 
-<section id="about" class="flex gap-3 items-center">
+<section id="about" class="flex gap-4 items-center">
 	<Angel />
 	<div class="flex flex-col items-center gap-6">
 		<h1 class="font-bold font-inter text-7xl text-center">{$t('Angel Ponce')}</h1>
@@ -11,12 +11,10 @@
 			{$t('“Las grandes oportunidades nacen de haber sabido aprovechar las pequeñas” ~ Bill Gates')}
 		</p>
 		<div class="flex items-center gap-4">
-			<Linkedin
-				className="grayscale hover:grayscale-0 transition-all duration-200 cursor-pointer"
-			/>
-			<Github className="opacity-60 hover:opacity-100 transition-all duration-200 cursor-pointer" />
-			<Gmail className="grayscale hover:grayscale-0 transition-all duration-200 cursor-pointer" />
-			<CV className="grayscale hover:grayscale-0 transition-all duration-200 cursor-pointer" />
+			<Linkedin className="cursor-pointer" />
+			<Github className="cursor-pointer" />
+			<Gmail className="cursor-pointer" />
+			<CV className="cursor-pointer" />
 		</div>
 	</div>
 </section>
