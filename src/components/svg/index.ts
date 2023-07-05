6f56f5995da27e6206647fbcbad87f4ca91841ab
Angel Ponce <acdpf000@gmail.com>
2023-07-05 15:48:59 -0600

    work experince svgs

--- src/components/svg/index.ts
+++ src/components/svg/index.ts
@@ -1,3 +1,6 @@
 import Angel from './Angel.svelte';
+import Finanssoreal from './Finanssoreal.svelte';
+import Pentcloud from './Pentcloud.svelte';
+import VSCode from './VSCode.svelte';
 
-export { Angel };
+export { Angel, Pentcloud, Finanssoreal, VSCode };
