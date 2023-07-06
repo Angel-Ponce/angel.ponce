54558a4a6dffdfadec886f77d19c4d1f08a6b11b
Angel Ponce <acdpf000@gmail.com>
2023-07-05 22:41:47 -0600

    using each to render projects

--- src/components/organisms/Projects.svelte
+++ src/components/organisms/Projects.svelte
@@ -1,13 +1,42 @@
+<script lang="ts" context="module">
+	interface Project {
+		title: string;
+		icon: typeof SvelteComponent<Record<string, any>>;
+		desc: string;
+		href?: string;
+	}
+</script>
+
 <script lang="ts">
 	import { IonIcon } from '$atoms';
 	import t from '$i18n/t';
-	import { Jequi } from '$svg';
-	import { onMount } from 'svelte';
+	import { DHondt, DynamicScheduleWeb, Jequi } from '$svg';
+	import { SvelteComponent, onMount } from 'svelte';
 	import { LinkOutline } from 'svelte-ionicons';
 	import type { SwiperOptions, Swiper } from 'swiper/types';
 
 	let swiperEl: any;
 	let swiper: Swiper;
+	const projects: Project[] = [
+		{
+			title: 'Jequi',
+			icon: Jequi,
+			desc: 'Una plataforma multi-empresarial que facilita la gestión de recursos humanos en todos los aspectos posibles.',
+			href: 'https://jequi.io'
+		},
+		{
+			title: 'Dynamic Schedule Web',
+			icon: DynamicScheduleWeb,
+			desc: 'Una plataforma útil para gestionar horarios estudiantiles y actividades personales.',
+			href: 'https://dynamic-schedule-web.vercel.app'
+		},
+		{
+			title: "D'Hondt Analizer",
+			icon: DHondt,
+			desc: 'Sistema de análisis de votación política para medir la cantidad de escaños obtenidos por partido.',
+			href: 'https://gt-votes.vercel.app'
+		}
+	];
 
 	onMount(() => {
 		Object.assign(swiperEl, {
@@ -36,29 +65,34 @@
 <section id="projects" class="flex w-full items-center flex-col gap-4">
 	<h2 class="font-bold font-inter text-left w-full text-2xl">{$t('Proyectos')}</h2>
 	<swiper-container bind:this={swiperEl} class="max-w-full w-full">
-		<swiper-slide>
-			<div class="flex w-full items-center gap-3">
-				<Jequi />
-				<h4 class="font-bold font-inter">Jequi</h4>
-			</div>
-			<p class="w-full text-justify">
-				Una plataforma multi-empresarial que facilita la gestión de recursos humanos en todos los
-				aspectos posibles.
-			</p>
-			<a
-				class="text-secondary w-full flex gap-3 items-center text-left text-base hover:underline underline-offset-2"
-				href="https://jequi.io"
-				target="_blank"
-			>
-				<IonIcon icon={LinkOutline} />Ver más
-			</a>
-		</swiper-slide>
-		<swiper-slide>Dynamic Schedule Web</swiper-slide>
-		<swiper-slide>D'hondt Analizer</swiper-slide>
+		{#each projects as project}
+			<swiper-slide>
+				<div class="flex w-full items-center gap-3">
+					<svelte:component this={project.icon} />
+					<h4 class="font-bold font-inter">{$t(project.title)}</h4>
+				</div>
+				<p class="w-full text-left">
+					{$t(project.desc)}
+				</p>
+				{#if project.href}
+					<div class="flex-1 w-full flex items-end pb-6">
+						<a
+							class="text-secondary w-full flex gap-3 items-center text-left text-base hover:underline underline-offset-2"
+							href={project.href}
+							target="_blank"
+						>
+							<IonIcon icon={LinkOutline} />Ver más
+						</a>
+					</div>
+				{/if}
+			</swiper-slide>
+		{/each}
+
+		<!-- <swiper-slide>D'hondt Analizer</swiper-slide>
 		<swiper-slide>CUNOR Lends</swiper-slide>
 		<swiper-slide>Svelte Reactions</swiper-slide>
 		<swiper-slide>ITIC</swiper-slide>
-		<swiper-slide>SIVOT Auxilio Póstumo</swiper-slide>
+		<swiper-slide>SIVOT Auxilio Póstumo</swiper-slide> -->
 	</swiper-container>
 </section>
 
--- src/components/svg/index.ts
+++ src/components/svg/index.ts
@@ -1,7 +1,9 @@
 import Angel from './Angel.svelte';
+import DHondt from './DHondt.svelte';
+import DynamicScheduleWeb from './DynamicScheduleWeb.svelte';
 import Finanssoreal from './Finanssoreal.svelte';
 import Jequi from './Jequi.svelte';
 import Pentcloud from './Pentcloud.svelte';
 import VSCode from './VSCode.svelte';
 
-export { Angel, Pentcloud, Jequi, Finanssoreal, VSCode };
+export { Angel, DHondt, DynamicScheduleWeb, Pentcloud, Jequi, Finanssoreal, VSCode };
