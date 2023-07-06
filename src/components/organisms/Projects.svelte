3f3bf3f10a13a113b851b8bd4984a4c6d9eac74c
Angel Ponce <acdpf000@gmail.com>
2023-07-05 21:16:03 -0600

    initial swiper component

--- src/components/organisms/Projects.svelte
+++ src/components/organisms/Projects.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
 	import t from '$i18n/t';
+	import { onMount } from 'svelte';
+	import type { SwiperOptions, Swiper } from 'swiper/types';
+
+	let swiperEl: any;
+	let swiper: Swiper;
+
+	onMount(() => {
+		Object.assign(swiperEl, {
+			pagination: true,
+			effect: 'coverflow',
+			grabCursor: true,
+			centeredSlides: true,
+			slidesPerView: 'auto',
+			coverflowEffect: {
+				rotate: 60,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: false
+			},
+			initialSlide: 1,
+			on: {
+				init: (sw) => {
+					swiper = sw;
+				}
+			}
+		} as SwiperOptions);
+	});
 </script>
 
 <section id="projects" class="flex w-full items-center flex-col gap-4">
 	<h2 class="font-bold font-inter text-left w-full text-2xl">{$t('Proyectos')}</h2>
+	<swiper-container bind:this={swiperEl} class="max-w-full w-full">
+		<swiper-slide class="w-full max-w-xs h-80 bg-grayblue bg-opacity-20">Slide 1</swiper-slide>
+		<swiper-slide class="w-full max-w-xs h-80 bg-grayblue bg-opacity-20">Slide 2</swiper-slide>
+		<swiper-slide class="w-full max-w-xs h-80 bg-grayblue bg-opacity-20">Slide 3</swiper-slide>
+	</swiper-container>
 </section>
